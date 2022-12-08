556afdaf36e20a669b117617988ac7cc7df15b61
Apricity <87al@fresnou.org>
2022-12-08 11:37:17 -0800

    fix missing images, change the color with type and css for add and edit forms

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -22,20 +22,20 @@ module.exports.updateCard = async function(req, res) {
             moveCost3a: `/images/${req.body.moveCost3a}.png`,
             moveName1: req.body.moveName1,
             moveDMG1: req.body.moveDMG1,
-            moveCost1b: req.body.moveCost1b,
-            moveCost2b: req.body.moveCost2b,
-            moveCost3b: req.body.moveCost3b,
+            moveCost1b: `/images/${req.body.moveCost1b}.png`,
+            moveCost2b: `/images/${req.body.moveCost2b}.png`,
+            moveCost3b: `/images/${req.body.moveCost3b}.png`,
             moveName2: req.body.moveName2,
             moveDMG2: req.body.moveDMG2,
-            Weak1: req.body.Weak1,
-            Weak2: req.body.Weak2,
-            Weak3: req.body.Weak3,
-            Res1: req.body.Res1,
-            Res2: req.body.Res2,
-            Res3: req.body.Res3,
-            RetreatCost1: req.body.RetreatCost1,
-            RetreatCost2: req.body.RetreatCost2,
-            RetreatCost3: req.body.RetreatCost3
+            Weak1: `/images/${req.body.Weak1}.png`,
+            Weak2: `/images/${req.body.Weak2}.png`,
+            Weak3: `/images/${req.body.Weak3}.png`,
+            Res1: `/images/${req.body.Res1}.png`,
+            Res2: `/images/${req.body.Res2}.png`,
+            Res3: `/images/${req.body.Res3}.png`,
+            RetreatCost1: `/images/${req.body.RetreatCost1}.png`,
+            RetreatCost2: `/images/${req.body.RetreatCost2}.png`,
+            RetreatCost3: `/images/${req.body.RetreatCost3}.png`
         },
         {
             where:
@@ -45,6 +45,78 @@ module.exports.updateCard = async function(req, res) {
 
 
         });
+    res.redirect('/')
+}
+
+module.exports.deleteCard = async function(req, res) {
+    await card.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+
+        })
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req, res){
+    const card = {
+        name: "",
+        hp: "",
+        type: "",
+        image: "",
+        moveCost1a: "",
+        moveCost2a: "",
+        moveCost3a: "",
+        moveName1: "",
+        moveDMG1: "",
+        moveCost1b: "",
+        moveCost2b: "",
+        moveCost3b: "",
+        moveName2: "",
+        moveDMG2: "",
+        Weak1: "",
+        Weak2: "",
+        Weak3: "",
+        Res1: "",
+        Res2: "",
+        Res3: "",
+        RetreatCost1: "",
+        RetreatCost2: "",
+        RetreatCost3: ""
+    };
+    res.render('add', {card, types});
+}
+
+module.exports.addCard = async function(req, res) {
+    const newPokemon = await Pokemon.create(
+        {
+            name: req.body.name,
+            hp: req.body.hp,
+            type: `/images/${req.body.type}.png`,
+            image: req.body.image,
+            moveCost1a: `/images/${req.body.moveCost1a}.png`,
+            moveCost2a: `/images/${req.body.moveCost2a}.png`,
+            moveCost3a: `/images/${req.body.moveCost3a}.png`,
+            moveName1: req.body.moveName1,
+            moveDMG1: req.body.moveDMG1,
+            moveCost1b: `/images/${req.body.moveCost1b}.png`,
+            moveCost2b: `/images/${req.body.moveCost2b}.png`,
+            moveCost3b: `/images/${req.body.moveCost3b}.png`,
+            moveName2: req.body.moveName2,
+            moveDMG2: req.body.moveDMG2,
+            Weak1: `/images/${req.body.Weak1}.png`,
+            Weak2: `/images/${req.body.Weak2}.png`,
+            Weak3: `/images/${req.body.Weak3}.png`,
+            Res1: `/images/${req.body.Res1}.png`,
+            Res2: `/images/${req.body.Res2}.png`,
+            Res3: `/images/${req.body.Res3}.png`,
+            RetreatCost1: `/images/${req.body.RetreatCost1}.png`,
+            RetreatCost2: `/images/${req.body.RetreatCost2}.png`,
+            RetreatCost3: `/images/${req.body.RetreatCost3}.png`
+        });
+    console.log(newPokemon)
     res.redirect('/')
 }
 // module.exports.viewAll = function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const cardController = require('../controllers/cardController')
 router.get('/', cardController.viewAll);
 router.get('/edit/:id', cardController.renderEditForm);
 router.post('/edit/:id', cardController.updateCard);
+router.get('/delete/:id', cardController.deleteCard);
+router.get('/add', cardController.renderAddForm);
+router.post('/add', cardController.addCard);
 module.exports = router;
