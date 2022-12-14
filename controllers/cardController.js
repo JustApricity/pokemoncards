ea8fa8c241857e3fef51fead0b64883b644aca94
Apricity <87al@fresnou.org>
2022-12-14 10:03:37 -0800

    fix the STUPID PICTURES GRAAAAAAH also add more cards

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,5 +1,5 @@
 const {Pokemon} = require('../models')
-let types = ['none', 'bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'fighting', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water']
+let types = ['none', 'bug', 'dark', 'dragon', 'electric', 'fairy', 'flying', 'fire', 'fighting', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water']
 // //    fix this and then display images
 module.exports.viewCards = async function(req, res) {
     let searchTypes = ['all']
@@ -7,7 +7,7 @@ module.exports.viewCards = async function(req, res) {
         searchTypes.push(types[i]);
     }
     let search
-    let searchType = req.query.type || 'all';
+    let searchType = req.query.types || 'all';
     let searchRandom = req.query.random || false;
     if (searchType === 'all') {
         search = await Pokemon.findAll();
@@ -41,24 +41,24 @@ module.exports.updateCard = async function(req, res) {
             type: req.body.type,
             image: req.body.image,
             moveCost1a: req.body.moveCost1a,
-            moveCost2a: req.body.moveCost2a,
-            moveCost3a: req.body.moveCost3a,
+            moveCost2a: checkNull(req.body.moveCost2a),
+            moveCost3a: checkNull(req.body.moveCost3a),
             moveName1: req.body.moveName1,
             moveDMG1: req.body.moveDMG1,
             moveCost1b: req.body.moveCost1b,
-            moveCost2b: req.body.moveCost2b,
-            moveCost3b: req.body.moveCost3b,
+            moveCost2b: checkNull(req.body.moveCost2b),
+            moveCost3b: checkNull(req.body.moveCost3b),
             moveName2: req.body.moveName2,
             moveDMG2: req.body.moveDMG2,
             Weak1: req.body.Weak1,
-            Weak2: req.body.Weak2,
-            Weak3: req.body.Weak3,
+            Weak2: checkNull(req.body.Weak2),
+            Weak3: checkNull(req.body.Weak3),
             Res1: req.body.Res1,
-            Res2: req.body.Res2,
-            Res3: req.body.Res3,
+            Res2: checkNull(req.body.Res2),
+            Res3: checkNull(req.body.Res3),
             RetreatCost1: req.body.RetreatCost1,
-            RetreatCost2: req.body.RetreatCost2,
-            RetreatCost3: req.body.RetreatCost3
+            RetreatCost2: checkNull(req.body.RetreatCost2),
+            RetreatCost3: checkNull(req.body.RetreatCost3)
         },
         {
             where:
@@ -120,22 +120,22 @@ module.exports.addCard = async function(req, res) {
             type: req.body.type,
             image: req.body.image,
             moveCost1a: `/images/${req.body.moveCost1a}.png`,
-            moveCost2a: `/images/${req.body.moveCost2a}.png`,
-            moveCost3a: `/images/${req.body.moveCost3a}.png`,
+            moveCost2a: checkNull(`/images/${req.body.moveCost2a}.png`),
+            moveCost3a: checkNull(`/images/${req.body.moveCost3a}.png`),
             moveName1: req.body.moveName1,
             moveDMG1: req.body.moveDMG1,
             moveCost1b: `/images/${req.body.moveCost1b}.png`,
-            moveCost2b: `/images/${req.body.moveCost2b}.png`,
-            moveCost3b: `/images/${req.body.moveCost3b}.png`,
+            moveCost2b: checkNull(`/images/${req.body.moveCost2b}.png`),
+            moveCost3b: checkNull(`/images/${req.body.moveCost3b}.png`),
             moveName2: req.body.moveName2,
             moveDMG2: req.body.moveDMG2,
-            Weak1: checkNull(`/images/${req.body.Weak1}.png`),
+            Weak1: (`/images/${req.body.Weak1}.png`),
             Weak2: checkNull(`/images/${req.body.Weak2}.png`),
             Weak3: checkNull(`/images/${req.body.Weak3}.png`),
-            Res1: checkNull(`/images/${req.body.Res1}.png`),
+            Res1: (`/images/${req.body.Res1}.png`),
             Res2: checkNull(`/images/${req.body.Res2}.png`),
             Res3: checkNull(`/images/${req.body.Res3}.png`),
-            RetreatCost1: checkNull(`/images/${req.body.RetreatCost1}.png`),
+            RetreatCost1: (`/images/${req.body.RetreatCost1}.png`),
             RetreatCost2: checkNull(`/images/${req.body.RetreatCost2}.png`),
             RetreatCost3: checkNull(`/images/${req.body.RetreatCost3}.png`)
         });
